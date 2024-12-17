fc63b3d8b1e7d1ae7cf05a270c1e7bec75ed85df
Emirhan Eren <emirhanneren52@outlook.com>
2024-12-17 22:31:43 +0300

    init AdminPanel

--- CarSalesAndService.Service/Concrete/Service.cs
+++ CarSalesAndService.Service/Concrete/Service.cs
@@ -1,10 +1,11 @@
 ﻿using CarSalesAndService.Data;
 using CarSalesAndService.Data.Concrete;
 using CarSalesAndService.Entities;
+using CarSalesAndService.Service.Abstract;
 
 namespace CarSalesAndService.Service.Concrete
 {
-    public class Service<T> : Repository<T> where T : class, IEntity, new()
+    public class Service<T> : Repository<T>,IService<T> where T : class, IEntity, new()
     {
         public Service(DatabaseContext context) : base(context)
         {
--- CarSalesAndService.WebUI/Program.cs
+++ CarSalesAndService.WebUI/Program.cs
@@ -28,6 +28,10 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
+            name: "adminPanel",
+            pattern: "{area:exists}/{controller=Main}/{action=Index}/{id?}" );
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
